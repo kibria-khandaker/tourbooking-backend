4bf1ac9a0991029d5bc8ab646cb3cc6a2c30b409
Golam Kibria <kibriakhandaker@gmail.com>
2023-02-10 04:18:01 +0600

    Database connection and Post data to MDB API Done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import tourRouter from './routes/tours.js'
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
@@ -14,21 +16,26 @@ const port = process.env.PORT || 8000;
 // })
 
 // Data base Connection
+mongoose.set("strictQuery", false)
 const connect = async () => {
     try {
-
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB database Connected');
     } catch (err) {
-
+        console.log('MongoDB database Connection Failed');
     }
 }
-
-
 
 // middleware 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use("/tours", tourRouter);
 
 app.listen(port, () => {
+    connect();
     console.log(`The server has started on port: ${port}`)
 });
